68718744bf286d613d11f1e6926f7f15273a40b4
Muskaan Shaikh <ymuskaanshaikh0440@gmail.com>
2024-04-14 21:37:31 +0530

    changetwo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Navbar.css'
+import logo from  '../../assets/logo.png'
 
 const Navbar = () => {
   return (
     <nav>
-      <img src="" alt="" />
+      <img src={logo}  alt=""  className='logo'/>
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About Us</li>
+        <li>Campus</li>
+        <li>Testimonials</li>
+        <li>Contact us</li>
+      </ul>
     </nav>
   )
 }
