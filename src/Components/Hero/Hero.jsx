5aa4d170179c952c4eba300c3e1229ab8af8d93c
Muskaan Shaikh <ymuskaanshaikh0440@gmail.com>
2024-04-15 15:57:57 +0530

    chang4

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Hero.css'
+import  dark_arrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div className='hero container'>
+        <div className="hero-text">
+            <h1>We Ensure better education for a better world</h1>
+            <p>Our cutting-edge curriculm is design to empower students 
+                with the knowlegde, skills and exeperiences needed to excel 
+                in the dynamic field of education</p>
+                <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button>
+        </div>
       
     </div>
   )
