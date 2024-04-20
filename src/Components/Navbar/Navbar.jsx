d944e527159b3123c1f94c3b8a5936043d818128
Muskaan Shaikh <ymuskaanshaikh0440@gmail.com>
2024-04-20 23:36:19 +0530

    navbar changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './Navbar.css';
-import logo from  '../../assets/logo.png';
+import logo from '../../assets/logo.png';
 import menu_icon from '../../assets/menu-icon.png';
-import { Link } from "react-scroll";
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
@@ -18,18 +18,22 @@ const Navbar = () => {
     setMobileMenu(!mobileMenu);
   };
 
+  const closeMobileMenu = () => {
+    setMobileMenu(false);
+  };
+
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       <img src={logo} alt="" className='logo' />
       <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu} />
       <ul className={mobileMenu ? 'show-mobile-menu' : ''}>
-        <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
-        <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
-        <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
-        <li><Link to='robotics' smooth={true} offset={-150} duration={500}>Robotics</Link></li>
-        <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
-        <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
-        <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
+        <li><Link to='hero' smooth={true} offset={0} duration={500} onClick={closeMobileMenu}>Home</Link></li>
+        <li><Link to='program' smooth={true} offset={-260} duration={500} onClick={closeMobileMenu}>Program</Link></li>
+        <li><Link to='about' smooth={true} offset={-150} duration={500} onClick={closeMobileMenu}>About Us</Link></li>
+        <li><Link to='robotics' smooth={true} offset={-150} duration={500} onClick={closeMobileMenu}>Robotics</Link></li>
+        <li><Link to='campus' smooth={true} offset={-260} duration={500} onClick={closeMobileMenu}>Campus</Link></li>
+        <li><Link to='testimonials' smooth={true} offset={-260} duration={500} onClick={closeMobileMenu}>Testimonials</Link></li>
+        <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn' onClick={closeMobileMenu}>Contact us</Link></li>
       </ul>
     </nav>
   );
